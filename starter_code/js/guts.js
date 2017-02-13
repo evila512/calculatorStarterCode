4139ec9f346989912eb1e5122ac5c9f0a3c22eda
Evila Santos <evilavsantos@gmail.com>
2017-02-13 23:12:19 +0000

    I made a change bro

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -5,33 +5,33 @@
 
 function divide(x,y)
 {
-	//Write your code below this line
+	return x/y;
 	
 }
 
 function multiply(x,y)
 {
-	
+	return x*y;
 }
 
 function subtract(x,y)
 {
-	
+	return x-y;
 }
 
 function add(x,y)
 {
-	
+	return x+y;
 }
 
 function power(x,y)
 {
-	
+	return x^y;
 }
 
 function square_root(x)
 {
-	
+    
 }
 
 function logBase10(x)
